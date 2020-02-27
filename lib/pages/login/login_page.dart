44014450a2c187e125d41137a261e3626048c9c0
Grabli66 <grabli66@gmail.com>
2020-02-27 07:54:54 +0300

    Login page state

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:team_lead/common/services/team_lead_service.dart';
+import 'package:team_lead/pages/login/stores/login_page_state_type.dart';
+import 'package:team_lead/pages/login/stores/login_page_store.dart';
 import 'package:team_lead/routes.dart';
 import 'package:team_lead/team_lead_app_font.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -17,62 +20,70 @@ class LoginPage extends StatefulWidget {
 
 /// Состояние страницы
 class _LoginPageState extends State<LoginPage> {
+  /// Состояние
+  final _pageState = LoginPageStore();
+
   /// Подключается с помощью google
-  void _signWithGoogle() async {
-    final googleUser = await teamLeadService.userService.loginGoogle();
-    final user =
-        await teamLeadService.userService.getUserInfoById(googleUser.id);
-    if (user == null) {
-      Navigator.popAndPushNamed(context, Routes.CreateUser,
-          arguments: googleUser);
-    } else {
-      await teamLeadService.userService.login(user.id);
-      Navigator.popAndPushNamed(context, Routes.PostList);
-    }
+  void _signWithGoogle() {
+    _pageState.loginGoogle(context);
   }
 
   /// Создаёт виджет
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: Container(
-                height: 500,
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 16),
-                      child: Icon(Icons.phone_android, size: 136),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 36),
-                      child: Text("Войти",
-                          style: TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.bold)),
-                    ),
-                    SizedBox(
-                      height: 48,
-                      child: RaisedButton.icon(
-                          icon: Icon(TeamLeadAppFont.google),
-                          color: Colors.red,
-                          label: Text("Подключиться через google"),
-                          onPressed: _signWithGoogle),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 24),
-                      child: SizedBox(
-                        height: 48,
-                        child: RaisedButton.icon(
-                            icon: Icon(TeamLeadAppFont.github_circled),
-                            color: Colors.blue,
-                            label: Text("Подключиться через github"),
-                            onPressed: () {
-                              Navigator.pushReplacementNamed(
-                                  context, Routes.PostList);
-                            }),
-                      ),
-                    ),
-                  ],
-                ))));
+    return Observer(builder: (context) {
+      switch (_pageState.state) {
+        case LoginPageStateType.Input:
+          return Scaffold(
+              body: Center(
+                  child: Container(
+                      height: 500,
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 16),
+                            child: Icon(Icons.phone_android, size: 136),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 36),
+                            child: Text("Войти",
+                                style: TextStyle(
+                                    fontSize: 24, fontWeight: FontWeight.bold)),
+                          ),
+                          SizedBox(
+                            height: 48,
+                            child: RaisedButton.icon(
+                                icon: Icon(TeamLeadAppFont.google),
+                                color: Colors.red,
+                                label: Text("Подключиться через google"),
+                                onPressed: _signWithGoogle),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 24),
+                            child: SizedBox(
+                              height: 48,
+                              child: RaisedButton.icon(
+                                  icon: Icon(TeamLeadAppFont.github_circled),
+                                  color: Colors.blue,
+                                  label: Text("Подключиться через github"),
+                                  onPressed: () {
+                                    Navigator.pushReplacementNamed(
+                                        context, Routes.PostList);
+                                  }),
+                            ),
+                          ),
+                        ],
+                      ))));
+        case LoginPageStateType.Login:
+          return Scaffold(
+              body: Center(
+            child: CircularProgressIndicator(),
+          ));
+        case LoginPageStateType.Error:
+          break;
+      }
+
+      return Row();
+    });
   }
 }
---
+++ lib/pages/login/stores/login_page_store.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/widgets.dart';
+import 'package:mobx/mobx.dart';
+import 'package:team_lead/common/services/team_lead_service.dart';
+import 'package:team_lead/pages/login/stores/login_page_state_type.dart';
+import 'package:team_lead/routes.dart';
+
+part 'login_page_store.g.dart';
+
+class LoginPageStore = _LoginPageStore with _$LoginPageStore;
+
+/// Модель окна входа
+abstract class _LoginPageStore with Store {
+  /// Состояние страницы
+  @observable
+  LoginPageStateType state = LoginPageStateType.Input;
+
+  /// Проводит подключение через
+  Future loginGoogle(BuildContext context) async {
+    state = LoginPageStateType.Login;
+    final googleUser = await teamLeadService.userService.loginGoogle();
+    final user =
+        await teamLeadService.userService.getUserInfoById(googleUser.id);
+    if (user == null) {
+      Navigator.popAndPushNamed(context, Routes.CreateUser,
+          arguments: googleUser);
+    } else {
+      await teamLeadService.userService.login(user.id);
+      Navigator.popAndPushNamed(context, Routes.PostList);
+    }
+  }
+}
